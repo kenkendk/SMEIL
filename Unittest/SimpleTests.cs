42ee40367440ce39fcaee8aa934a9fc2f404956b
Kenneth Skovhede <kenneth@hexad.dk>
2019-02-04 17:17:24 +0100

    Added some more tests

--- Unittest/SimpleTests.cs
+++ Unittest/SimpleTests.cs
@@ -11,16 +11,55 @@ namespace Unittest
         [TestMethod]
         public void TestAdderTyped()
         {
-            TestAdderTyped_core("../../../adder_typed.sme");
+            TestAdder_core("../../../adder_typed.sme");
         }
 
         [TestMethod]
         public void TestAdderUntyped()
         {
-            TestAdderTyped_core("../../../adder_untyped.sme");
+            TestAdder_core("../../../adder_untyped.sme");
         }
 
-        private void TestAdderTyped_core(string path)
+        [TestMethod]
+        public void TestAdderIdUntyped()
+        {
+            TestAdderId_core("../../../adder_id_untyped.sme");
+        }
+
+        [TestMethod]
+        public void TestAdderIdTyped()
+        {
+            TestAdderId_core("../../../adder_id_typed.sme");
+        }
+
+        private void TestAdderId_core(string path)
+        {
+            var state = Loader.LoadModuleAndImports(path, null, null);
+            state.Validate();
+
+            Assert.AreEqual(1, state.AllInstances.OfType<SMEIL.Parser.Instance.Network>().Count());
+            Assert.AreEqual(2, state.AllInstances.OfType<SMEIL.Parser.Instance.Process>().Count());
+
+            var mainproc = state.AllInstances.OfType<SMEIL.Parser.Instance.Process>().First(x => x.SourceName == "id");
+            var scope = state.LocalScopes[mainproc];
+            var inbus = state.FindSymbol("inbus", scope) as SMEIL.Parser.Instance.Bus
+                ?? throw new ArgumentException("Input bus instance was of wrong type");
+            var outbus = state.FindSymbol("outbus", scope) as SMEIL.Parser.Instance.Bus
+                ?? throw new ArgumentException("Output bus instance was of wrong type");
+            var target = state.ResolveTypeName("tdata", scope)
+                ?? throw new ArgumentException("tdata definition was of wrong type");
+
+            Assert.AreEqual(inbus, state.FindSymbol("plusone_net.id_inst.inbus", state.LocalScopes[state.TopLevel.Module]));
+            Assert.AreEqual(outbus, state.FindSymbol("plusone_net.id_inst.outbus", state.LocalScopes[state.TopLevel.Module]));
+
+            var inbus_type = inbus.ResolvedType;
+            var outbus_type = inbus.ResolvedType;
+
+            Assert.IsTrue(object.Equals(inbus_type, target));
+            Assert.IsTrue(object.Equals(outbus_type, target));
+        }
+
+        private void TestAdder_core(string path)
         {
             var state = Loader.LoadModuleAndImports(path, null, null);
             state.Validate();
