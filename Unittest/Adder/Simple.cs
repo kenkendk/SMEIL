086014273c09e82c40c8a6e11032eb96744ddf3e
Kenneth Skovhede <kenneth@hexad.dk>
2019-11-13 10:38:21 +0100

    Added tests for parameterized processes

--- Unittest/Adder/Simple.cs
+++ Unittest/Adder/Simple.cs
@@ -57,6 +57,22 @@ namespace Unittest.Adder
             TestNameReuse("../../../smeil/simple/internal_name_reuse.sme");
         }
 
+        [TestMethod]
+        public void TestParameterizedFunctions()
+        {
+            TestParameterized("../../../smeil/simple/adder_parameterized.sme");
+        }
+
+        public static void TestParameterized(string path)
+        {
+            var state = Loader.LoadModuleAndImports(path, null, null);
+            state.Validate();
+
+            Assert.AreEqual(1, state.AllInstances.OfType<SMEIL.Parser.Instance.Network>().Count());
+            Assert.AreEqual(2, state.AllInstances.OfType<SMEIL.Parser.Instance.Process>().Count());
+
+            GenerateVHDLAndVerify(path);
+        }
         public static void TestNameReuse(string path)
         {
             var state = Loader.LoadModuleAndImports(path, null, null);
